7d42679a8c9d85c90e5ec8456441142202c56cd4
chauchausoup <7krishna7dhakal7@gmail.com>
2020-09-01 23:41:41 +0545

    api

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //entry point of our app
+var express = require('express');
+var http = require('http');
+var fs = require('fs')
+
+
+var app = express();
+var server = http.createServer(app);
+
+
+app.get('/',(req,res)=>{
+    res.send(`<h1>it works</h1>`);
+})
+
+app.get('/places',(req,res)=>{
+    fs.readFile('./db.json',(err,data)=>{
+        res.send(data.toString())
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log(`Server is listening on port ${3000}!!!`)
+})
+
